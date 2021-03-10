4eac09bf6f10032edb5c8da239e77ab7d74c04b5
yeyiwei <1353263078@qq.com>
2021-03-10 16:23:29 +0800

    workflow8

--- run.bat
+++ run.bat
@@ -5,5 +5,6 @@ if not exist D:\temp ( mkdir D:\temp )
 if exist D:\temp\test-1.0-SNAPSHOT.jar del D:\temp\test-1.0-SNAPSHOT.jar
 ::复制文件
 copy C:\actions-runner\test-work\test\test\target\test-1.0-SNAPSHOT.jar D:\temp
-cd D:\temp
-java -jar test-1.0-SNAPSHOT.jar > D:\temp\test.log &
+::编译机用PowerShell执行，需要让编译机的PowerShell切换到目录D:\temp，下面的命令会要求编译机在启动编译机而不是切换目录，应该写到.github里
+::cd D:\temp
+::java -jar test-1.0-SNAPSHOT.jar > D:\temp\test.log &
