99f8b59f879e290269771d18fc93e5c07f709d8b
yeyiwei <1353263078@qq.com>
2021-03-10 15:13:57 +0800

    workflow8

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 @echo off
-::删除可能存在的过期文件
-if exist D:\temp\test-1.0-SNAPSHOT.jar del D:\temp\test-1.0-SNAPSHOT.jar
 ::判断文件夹是否存在
 if not exist D:\temp ( mkdir D:\temp )
+::删除可能存在的过期文件
+if exist D:\temp\test-1.0-SNAPSHOT.jar del D:\temp\test-1.0-SNAPSHOT.jar
 ::复制文件
 copy C:\actions-runner\test-work\test\test\target\test-1.0-SNAPSHOT.jar D:\temp
 cd D:\temp
