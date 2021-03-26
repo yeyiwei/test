7f5008d7fea628a330b24ff2e2aa3318256a75d7
yeyiwei <1353263078@qq.com>
2021-03-26 17:28:39 +0800

    master push

--- .github/workflows/deployment.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title deployment.bat
-echo deployment project
-if not exist ..\..\..\..\..\..\temp ( mkdir ..\..\..\..\..\..\temp )
-if exist ..\..\..\..\..\..\temp\test-1.0-SNAPSHOT.jar del ..\..\..\..\..\..\temp\test-1.0-SNAPSHOT.jar
-echo befor copy
-copy ..\..\target\test-1.0-SNAPSHOT.jar ..\..\..\..\..\..\temp\test-1.0-SNAPSHOT.jar
